5039b16eea3fa5d44c39c840e6d6069c1d8bd399
rnewstead1 <racheljnewstead@gmail.com>
2016-11-21 23:23:12 +0000

    Fix static asset location

--- src/app/index.js
+++ src/app/index.js
@@ -20,12 +20,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
+  src: path.join(__dirname, '../../public'),
+  dest: path.join(__dirname, '../../public'),
   indentedSyntax: true,
   sourceMap: true
 }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../../public')));
 
 routes(app);
 
