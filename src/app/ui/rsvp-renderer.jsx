dfd7a34314d8531e06e4dfcd84f2aa4aa5f9ef15
rnewstead1 <racheljnewstead@gmail.com>
2016-11-23 22:29:48 +0000

    Add redux-logger

--- src/app/ui/rsvp-renderer.jsx
+++ src/app/ui/rsvp-renderer.jsx
@@ -1,14 +1,15 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const { Provider } = require('react-redux');
-const { createStore, combineReducers } = require('redux');
+const { createStore, combineReducers, applyMiddleware } = require('redux');
 const { reducer: reduxFormReducer } = require('redux-form');
+const createLogger = require('redux-logger');
 const RSVP = require('./RSVP.jsx').default;
 
 const reducer = combineReducers({
   form: reduxFormReducer
 });
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(createLogger()));
 
 const showResults = (values) => {
   console.log(`Submitted:\n\n${JSON.stringify(values)}`);
