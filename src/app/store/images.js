d5011ec380b67cd26c18aee5dc71d7078d6d6b3b
Will Normand <mail@wnormand.co.uk>
2017-01-14 18:24:30 +0000

    Put images from DB in separate directory
    Makes for easier .gitignoring.

--- src/app/store/images.js
+++ src/app/store/images.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
 
+const imagesPath = 'public/images/db';
+
 module.exports.writeFilesFromDb = (db) => {
+  if (!fs.existsSync(imagesPath)) {
+    fs.mkdirSync(imagesPath);
+  }
+
   const images = db.collection('images');
   return images.find().toArray()
     .then(docs => docs.forEach((doc) => {
-      const path = `public/images/${doc.name}`;
-      console.log('Writing image file from database:', path);
+      const path = `${imagesPath}/${doc.name}`;
+      console.log('Writing image file from database to', path);
       fs.writeFileSync(path, doc.data.buffer);
     }))
     .catch(err => console.error('Error saving image files', err));
